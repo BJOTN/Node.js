197191e788ec5b93821e8a3e801959ef570dcc47
Bjorn Agbalog <bjornlouie98@gmail.com>
2024-01-11 12:38:27 +0100

    ex

--- src/controller/planets.ts
+++ src/controller/planets.ts
@@ -1,30 +1,28 @@
 import { Request, Response } from "express";
+import pgPromise from "pg-promise";
 
-type Planet = {
-  id: number;
-  name: string;
+const db = pgPromise()("postgres://postgres:postgres@localhost:5432/planets");
+
+/* SetUp db */
+const setUpDb = async () => {
+  await db.none(`
+  DROP TABLE IF EXIST planets
+  CREATE TABLE plantes(
+    ID SERIAL NOT NULL PRIMARY KEY
+    NAME TEXT NOT NULL
+  )`);
+  await db.none(`INSERT INTO planets(none)
+  VALUE('Earth')`);
 };
 
-type Planets = Planet[];
-
-let planets: Planets = [
-  {
-    id: 1,
-    name: "Earth",
-  },
-  {
-    id: 2,
-    name: "Mars",
-  },
-];
-
-const getAll = (req: Request, res: Response) => {
+const getAll = async(req: Request, res: Response) => {
+  const planets = await db.many(`SELECT * FROM planets;`);
   res.status(200).json(planets);
 };
 
-const getOnebyID = (req: Request, res: Response) => {
+const getOnebyID = async (req: Request, res: Response) => {
   const planetID = Number(req.params.id);
-  const planet = planets.find((p) => p.id === planetID);
+  const planet = await db.one(`SELECT * FROM planets WHERE id=$1;`, Number(planetID));
   if (!planet) {
     return res.status(404).json({ error: "Planet dont exist" });
   }
@@ -35,7 +33,7 @@ const create = (req: Request, res: Response) => {
   const { id, name } = req.body;
   const newPlanet = { id, name };
 
-  planets.push(newPlanet);
+  /* planets.push(newPlanet); */
 
   res.status(201).json({ msg: "planet created" });
 };
@@ -43,14 +41,14 @@ const create = (req: Request, res: Response) => {
 const updatebyID = (req: Request, res: Response) => {
   const planetId = Number(req.params.id);
   const { name } = req.body;
-  planets = planets.map((p) => (p.id === planetId ? { ...p, name } : p));
-  res.status(200).json({ msg: "planet modified" });
+/*   planets = planets.map((p) => (p.id === planetId ? { ...p, name } : p));
+ */  res.status(200).json({ msg: "planet modified" });
 };
 
 const delatebyID = (req: Request, res: Response) => {
   const planetID = Number(req.params.id);
-  planets = planets.filter((p) => p.id !== planetID);
-  res.status(200).json({ msg: "planet delated" });
+/*   planets = planets.filter((p) => p.id !== planetID);
+ */  res.status(200).json({ msg: "planet delated" });
 };
 
-export { getAll, create, delatebyID, updatebyID , getOnebyID};
+export { getAll, create, delatebyID, updatebyID, getOnebyID };
