41d0b4bca1d89933d696dec6a87dd7a6528599fa
Bjorn Agbalog <bjornlouie98@gmail.com>
2024-01-21 16:07:15 +0100

    excercise

---
+++ passport.ts
@@ -0,0 +1,27 @@
+import * as dotenv from 'dotenv';
+import passportJWT from 'passport-jwt';
+import passport from 'passport';
+import {db} from './db'
+dotenv.config();
+
+const { SECRET } = process.env;
+
+
+passport.use(
+  new passportJWT.Strategy(
+    {
+      secretOrKey: SECRET,
+      jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeaderAsBearerToken(),
+    },
+    async (payload, done) => {
+      // Implementa la logica di verifica del token JWT qui
+      const user = db.one('SELECT * FROM users WHERE id=$1', payload.id)
+
+      try {
+        return user ? done(null, user) : done(new Error('user not found'))
+      } catch (error) {
+        done(error)
+      }
+    }
+  )
+);
